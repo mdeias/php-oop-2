f123d400977aebcbda885f59da67e26ca0b1946b
mdeias <mdeias@icloud.com>
2022-01-18 17:13:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . "/classes/User.php";
+require_once __DIR__ . "/classes/Product.php";
+require_once __DIR__ . "/classes/CreditCard.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+</head>
+<body>
+    
+</body>
+</html>
